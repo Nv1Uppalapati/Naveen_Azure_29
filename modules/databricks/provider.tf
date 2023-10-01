3205000fa0054b4ac995fde12be3a3d07691e74f
Naveen Uppalapati <135144798+Nv1Uppalapati@users.noreply.github.com>
2023-10-01 12:29:01 -0500

    Update provider.tf

--- modules/databricks/provider.tf
+++ modules/databricks/provider.tf
@@ -7,19 +7,6 @@ terraform {
   }
 }
 
-provider "databricks" {
-    azure_workspace_resource_id = azure_databricks_workspace.workspace.id
-    azure_client_id = "2c754c8a-3971-4c72-892f-e6c4eb0eb67c"
-    azure_clinet_secret = "LXJ8Q~8xiratJFMrdDnJ-WzfIRbI6cPMdGfZCaXr"
-    azure_tenant_id = "20daed00-4d24-4bb4-9072-54966122bed5"
-}
 
-
-provider "azurerm" { 
-    features {}
-    client_id = "2c754c8a-3971-4c72-892f-e6c4eb0eb67c"
-    clinet_secret = "LXJ8Q~8xiratJFMrdDnJ-WzfIRbI6cPMdGfZCaXr"
-    tenant_id = "20daed00-4d24-4bb4-9072-54966122bed5"
-    subscription_id = "bbf05370-7c0e-4f8c-a2c5-c51ead4d008f"
 
 }
